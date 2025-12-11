5fbeeead6fa38c3b034f82ee6447a3d462943178
Tom Rijnbeek <tom@tomrijnbeek.me>
2025-12-11 19:42:49 +0100

    🪽 Restore jetpack recipes

--- kubejs/startup_scripts/constants.js
+++ kubejs/startup_scripts/constants.js
@@ -1,9 +1,6 @@
 global.REMOVE_ITEMS = [
   'ftbfiltersystem:smart_filter',
 
-  'create_sa:brass_jetpack_chestplate',
-  'create_sa:andesite_jetpack_chestplate',
-  'create_sa:copper_jetpack_chestplate',
   'create_sa:fan_component',
   'create_sa:vault_component',
   'create_sa:block_picker',
