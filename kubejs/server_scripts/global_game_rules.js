d6341c8759cd2bfce1a32624eaa5de279a665eae
Tom Rijnbeek <tom@tomrijnbeek.me>
2025-12-14 10:19:00 +0100

    😴 Remove sleeping percentage rule

--- kubejs/server_scripts/global_game_rules.js
+++ kubejs/server_scripts/global_game_rules.js
@@ -1,5 +1,4 @@
 ServerEvents.loaded(e => {
   e.server.gameRules.set("doPatrolSpawning", false)
-  e.server.gameRules.set("playersSleepingPercentage", 50)
   e.server.gameRules.set("doTraderSpawning", false)
 })
